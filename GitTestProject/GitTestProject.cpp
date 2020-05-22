0c55bf8e66145c4fe18fd941e42c9965aa3e030f
Wyzzarde <boruchzadok@gmail.com>
2020-05-22 16:56:31 +0100

    Test Header added

--- GitTestProject/GitTestProject.cpp
+++ GitTestProject/GitTestProject.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <fstream>
 
 int main()
 {
---
+++ GitTestProject/test_header.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	//blip - This text tests the first NMR program git commit.
+}
