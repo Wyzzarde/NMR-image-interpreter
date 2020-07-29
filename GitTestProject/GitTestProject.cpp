636e2ab7d9cf3851818b737121b91d5406858508
Wyzzarde <boruchzadok@gmail.com>
2020-07-29 16:09:40 +0100

    Added edge highlighter - unfinished Added vector to csv writer - finished

--- GitTestProject/GitTestProject.cpp
+++ GitTestProject/GitTestProject.cpp
@@ -8,9 +8,6 @@
 
 
 #include "test_header.h"
-
-#include <fstream>
-#include <string>
 #include <cerrno>
 
 //This function takes a filename (string) and returns the contents of the file in the form of a string.
@@ -55,9 +52,11 @@ void BmpInputBuffer::bufferReset() {
 	stringIndex = 0;
 }
 
+//////////-------------------------------------------------------------------------------------------------
 
 class Bitmap {
 	private:
+		//blah is a placeholder variable, affects sod all
 		int blah = 9;
 
 	public:
@@ -66,7 +65,11 @@ class Bitmap {
 		int height = 0;
 	
 		Bitmap(std::string);
-		
+		int getValueAt (int x, int y);
+		void changeValueAt(int x, int y, int to_value);
+		void printMapToCSV ();
+
+		//need function to grab and edit cells
 		
 
 
@@ -128,17 +131,122 @@ Bitmap::Bitmap(std::string filename) {//constructor
 
 	std::cout << "vector shape - " << std::to_string(bitmap_values.size()) << "x" << std::to_string(bitmap_values.at(0).size());
 
-	std::cout << "\n" << std::to_string(width) << "x" << std::to_string(height);
+	std::cout << "\n" << std::to_string(width) << "x" << std::to_string(height) << "\n";
 	
 }
 
-
-
-int main()
-{	
-	Bitmap newMap("test-csv.csv");
+int Bitmap::getValueAt(int x, int y) {
+	return bitmap_values.at(x).at(y);
 }
 
+void Bitmap::changeValueAt(int x, int y, int to_value) {
+	bitmap_values.at(x).at(y) = to_value;
+}
+
+void Bitmap::printMapToCSV () {
+	
+	int arraySize = (width * height * 4) + width;
+	int arrayIterator = 0;
+	
+	char * preStringPoint;
+	preStringPoint = new char[arraySize];
+
+	std::string cellBuffer;
+	cellBuffer.reserve(10);
+
+	int iterator_number = 0;
+
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			iterator_number++;
+			//std::cout << std::to_string(x) << "," << std::to_string(y) << "\n";
+			cellBuffer = std::to_string(bitmap_values.at(x).at(y));
+			for (int i = 0; i < cellBuffer.size(); i++) {
+				preStringPoint[arrayIterator] = cellBuffer[i];
+				arrayIterator++;
+			}
+			if (x == width - 1) {
+				preStringPoint[arrayIterator] = '\n';
+				arrayIterator++;
+				if (y == height - 1) {
+					preStringPoint[arrayIterator] = NULL;
+					break;
+				}
+			}
+			else {
+				preStringPoint[arrayIterator] = ',';
+				arrayIterator++;
+			}
+		}
+	}
+
+	std::cout << "done!";
+	std::string outputString = std::string(preStringPoint);
+		
+	std::ofstream outFile;
+	outFile.open("csv_edges.csv");
+	outFile << outputString;
+	outFile.close();
+}
+
+////---------------------------------------------------------------------------------------------------------
+
+int inkThreshold = 120;
+//int bufferSize = 5;
+
+void highlightEdges(Bitmap inputMap) {
+	for (int x = 0; x < inputMap.width; x++) {
+		for (int y = 0; y < inputMap.height; y++) {
+			
+			int pointValue = inputMap.getValueAt(x, y);
+			bool edge = false;
+			
+			if (pointValue <= inkThreshold) {
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; i++) {
+						
+						if (inputMap.getValueAt(x+i, y+j) >= 120) {
+							edge = true;
+							break;
+						}
+					}
+					break;
+				}
+			}
+
+			if (edge != true) {
+				inputMap.changeValueAt(x, y, 300);
+			}
+		}
+	}
+	for (int x = 0; x < inputMap.width; x++) {
+		for (int y = 0; y < inputMap.height; y++) {
+			
+			int pointValue = inputMap.getValueAt(x, y);
+			
+			if (pointValue >= 300) {
+				inputMap.changeValueAt(x, y, 200);
+			}
+		}
+	}
+}
+
+void testAlgorithms(Bitmap map) {
+	map.changeValueAt(0, 0, 1234);
+	std::cout << std::to_string(map.getValueAt(0, 0)) << "-2\n";
+}
+
+int main()
+{
+	Bitmap newMap("test-csv.csv");
+	highlightEdges(newMap);
+	newMap.changeValueAt(0, 0, 201);
+	std::cout << std::to_string(newMap.getValueAt(0,0)) << "-1\n";
+	testAlgorithms(newMap);
+	std::cout << std::to_string(newMap.getValueAt(0, 0)) << "-36\n";
+	//newMap.printMapToCSV();
+
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
